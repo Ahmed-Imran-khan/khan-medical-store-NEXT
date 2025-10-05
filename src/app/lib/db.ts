09a5e4e967d6fd8b75cc8a2d6487f9fe2cd0916b
Ahmed Imran <ahmedimrankhaan@gmail.com>
2025-10-05 22:25:49 +0500

    Db code

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -5,18 +5,21 @@ declare global {
 }
 
 const pool =
-  global._pool ??
+  globalThis._pool ??
   mysql.createPool({
     host: process.env.DB_HOST!,
     user: process.env.DB_USER!,
     password: process.env.DB_PASSWORD!,
     database: process.env.DB_NAME!,
-    port: Number(process.env.DB_PORT) || 3306,
+    port: Number(process.env.DB_PORT ?? 3306),
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
-if (process.env.NODE_ENV !== "production") global._pool = pool;
+if (process.env.NODE_ENV !== "production") globalThis._pool = pool;
 
 export default pool;
