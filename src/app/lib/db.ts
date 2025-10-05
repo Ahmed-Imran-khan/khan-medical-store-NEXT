98f4572d7f5d7389e7d321365996f8a88af767ef
Ahmed Imran <ahmedimrankhaan@gmail.com>
2025-10-05 14:36:45 +0500

    Final change

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,25 +1,8 @@
-// import mysql from "mysql2/promise";
+import mysql from "mysql2/promise";
 
-// export const database = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "kms_store",
-// });
-
-// app/lib/db.ts (or equivalent file)
-// app/lib/db.ts
-
-import { Pool } from 'pg';
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, 
-  },
+export const database = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "kms_store",
 });
-
-// Simple wrapper to run queries
-export async function sql(query: string, values?: any[]) {
-    return await pool.query(query, values);
-}
