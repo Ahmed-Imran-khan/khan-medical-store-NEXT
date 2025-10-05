d5bb16384ff53bed251c939f403e60c7d1d56d78
Ahmed Imran <ahmedimrankhaan@gmail.com>
2025-10-05 22:42:01 +0500

    Db code

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,25 +1,8 @@
 import mysql from "mysql2/promise";
 
-declare global {
-  var _pool: mysql.Pool | undefined;
-}
-
-const pool =
-  globalThis._pool ??
-  mysql.createPool({
-    host: process.env.DB_HOST!,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
-    port: Number(process.env.DB_PORT ?? 3306),
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-
-if (process.env.NODE_ENV !== "production") globalThis._pool = pool;
-
-export default pool;
+export const database = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "kms_store",
+});
